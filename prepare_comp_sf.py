b9da6132e002c4ed061c1854d1976054c7d35d3a
Leslie <30477279+tgycmzwj@users.noreply.github.com>
2023-04-29 14:23:14 -0500

    na

--- prepare_comp_sf.py
+++ prepare_comp_sf.py
@@ -4,10 +4,12 @@ import time
 
 def prepare_comp_sf(conn,cursor,queries,freq="m"):
     print("Starting processing freq={freq} at time ".format(freq=freq) + time.asctime())
-    #query1
-    cursor.execute(queries["query1"].format(freq=freq))
-    cursor.fetchall()
-    print("finished query 1 at time "+time.asctime())
+
+    # #query1
+    # cursor.execute(queries["query1"].format(freq=freq))
+    # cursor.fetchall()
+    # print("finished query 1 at time "+time.asctime())
+
     #query2
     cursor.execute(queries["query2"].format(idvar="gvkey",datevar="datadate"))
     cursor.fetchall()
