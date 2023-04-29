de09e3dcac50d2379eccb8248ca2f72822187cca
Leslie <30477279+tgycmzwj@users.noreply.github.com>
2023-04-29 16:10:48 -0500

    na

--- prepare_comp_sf.py
+++ prepare_comp_sf.py
@@ -23,9 +23,9 @@ def prepare_comp_sf(conn,cursor,queries,freq="m"):
     # cursor.fetchall()
     # print("finished query 3_1 at time "+time.asctime())
 
-    # cursor.execute(queries["query3_2"])
-    # cursor.fetchall()
-    # print("finished query 3_2 at time "+time.asctime())
+    cursor.execute(queries["query3_2"])
+    cursor.fetchall()
+    print("finished query 3_2 at time "+time.asctime())
 
     cursor.execute(queries["query3_3"])
     cursor.fetchall()
