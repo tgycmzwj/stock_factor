96976248991ac3428c88d18927702b17ff8a66e5
tgycmzwj <30477279+tgycmzwj@users.noreply.github.com>
2023-05-04 13:05:43 -0500

    helper function

--- helper_func.py
+++ helper_func.py
@@ -4,15 +4,18 @@ import calendar
 import typing
 
 def intnx_(date:str,n:int,delta:str,alignment:str)->str:
-    if delta.lower()=="month":
-        date_after=datetime.datetime.strptime(date,"%Y-%m-%d")+relativedelta(months=n)
-    elif delta.lower()=="day":
-        date_after=datetime.datetime.strptime(date,"%Y-%m-%d")+relativedelta(days=n)
-    if alignment.lower()=="end":
-        day=calendar.monthrange(date_after.year,date_after.month)[1]
-        return datetime.datetime(date_after.year,date_after.month,day).strftime("%Y-%m-%d")
-    else:
-        return date_after.strftime("%Y-%m-%d")
+    try:
+        if delta.lower()=="month":
+            date_after=datetime.datetime.strptime(date,"%Y-%m-%d")+relativedelta(months=n)
+        elif delta.lower()=="day":
+            date_after=datetime.datetime.strptime(date,"%Y-%m-%d")+relativedelta(days=n)
+        if alignment.lower()=="end":
+            day=calendar.monthrange(date_after.year,date_after.month)[1]
+            return datetime.datetime(date_after.year,date_after.month,day).strftime("%Y-%m-%d")
+        else:
+            return date_after.strftime("%Y-%m-%d")
+    except:
+        return "null"
 def prepare_helper_func(conn):
     conn.create_function("INTNX_",4,intnx_)
     print("finished creating helper functions")
