18dfbbab3366e9b20a64b3d43d91191eb473703e
tgycmzwj <30477279+tgycmzwj@users.noreply.github.com>
2023-05-04 19:51:11 -0500

    duplicate on

--- prepare_comp_sf.py
+++ prepare_comp_sf.py
@@ -7,16 +7,16 @@ def prepare_comp_sf(conn,cursor,queries,freq="m"):
     util_funcs = utils.utils(conn,cursor)
     print("Starting processing freq={freq} at time ".format(freq=freq) + time.asctime())
 
-    #query1:
-    cursor.execute(queries["query1"].format(freq=freq))
-    cursor.fetchall()
-    print("finished query 1 at time "+time.asctime())
-    util_funcs.sort_and_remove_duplicates(table_in="__firm_shares1",table_out="__temp",sortvar="gvkey,datadate DESC",idvar="gvkey,datadate")
+    # #query1:
+    # cursor.execute(queries["query1"].format(freq=freq))
+    # cursor.fetchall()
+    # print("finished query 1 at time "+time.asctime())
+    # util_funcs.sort_and_remove_duplicates(table_in="__firm_shares1",table_out="__temp",sortvar="gvkey,datadate DESC",idvar="gvkey,datadate")
 
-    #query2
-    cursor.execute(queries["query2"])
-    cursor.fetchall()
-    print("finished query 2 at time "+time.asctime())
+    # #query2
+    # cursor.execute(queries["query2"])
+    # cursor.fetchall()
+    # print("finished query 2 at time "+time.asctime())
 
     #query3
     cursor.execute(queries["query3"].format(freq=freq))
