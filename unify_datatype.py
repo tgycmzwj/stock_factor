dbe8c23570fc34d157fb3b1517a0ef3ee2428cd9
tgycmzwj <30477279+tgycmzwj@users.noreply.github.com>
2023-05-10 14:55:23 -0500

    data type change

--- unify_datatype.py
+++ unify_datatype.py
@@ -1,5 +1,6 @@
 import time
 import utils
+import re
 
 def unify_datatype(conn,cursor):
     util_funcs=utils.utils(conn,cursor)
@@ -7,16 +8,11 @@ def unify_datatype(conn,cursor):
 
     for table_name in all_tables:
         all_columns=util_funcs.list_column(table_name=table_name)
-        # gvkey to integer
-        if "gvkey" in all_columns:
-            util_funcs.change_column_type(table_name=table_name,column_name="gvkey",column_type="Integer")
-        # permno to integer
-        if "permno" in all_columns:
-            util_funcs.change_column_type(table_name=table_name,column_name="permno",column_type="Integer")
-        # permco to integer
-        if "permco" in all_columns:
-            util_funcs.change_column_type(table_name=table_name,column_name="permco",column_type="Integer")
-        # date to string
-        if "datadate" in all_columns:
-            util_funcs.change_column_type(table_name=table_name,column_name="datadate",column_type="TEXT")
+        for column_name in all_columns:
+            # gvkey to integer
+            if column_name in ["gvkey","permno","permco"]:
+                util_funcs.change_column_type(table_name=table_name,column_name=column_name,column_type="Integer")
+            # date to string
+            if re.match(column_name,".^date$"):
+                util_funcs.change_column_type(table_name=table_name,column_name=column_name,column_type="TEXT")
 
