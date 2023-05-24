6a8160a6addc2a7fed6c9c50073900bb5b545534
tgycmzwj <30477279+tgycmzwj@users.noreply.github.com>
2023-05-24 05:54:48 -0500

    run prepare comp sf query11

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from typing import List
 import pandas as pd
 from query_storage import query_storage
-
+import time
 
 class utils(object):
     def __init__(self,conn,cursor):
@@ -15,7 +15,7 @@ class utils(object):
         for table in tables:
             self.cursor.execute(query.format(table_name=table))
             self.cursor.fetchall()
-            print("finished deleting table {}".format(table))
+            print("finished deleting table {} at time {}".format(table,time.asctime()))
         print("finished deleting all tables")
 
     def delete_column(self,table_columns:List[List[str]]):
@@ -24,7 +24,7 @@ class utils(object):
             self.cursor.execute(query.format(table_name=table,column_name=column))
             self.cursor.fetchall()
             self.conn.commit()
-            print("finished deleting column {} in table {}".format(column,table))
+            print("finished deleting column {} in table {} at time {}".format(column,table,time.asctime()))
         print("finished deleting all columns")
 
     def rename_table(self,tables:List[List[str]]):
@@ -33,7 +33,7 @@ class utils(object):
             self.cursor.execute(query.format(table_name_old=table_name_old,table_name_new=table_name_new))
             self.cursor.fetchall()
             self.conn.commit()
-            print("finished renaming table {} to {}".format(table_name_old,table_name_new))
+            print("finished renaming table {} to {} at time {}".format(table_name_old,table_name_new,time.asctime()))
         print("finished renaming all tables")
 
     def rename_column(self,table_columns:List[List[str]]):
@@ -42,7 +42,7 @@ class utils(object):
             self.cursor.execute(query.format(table_name=table,column_name_old=column_name_old,column_name_new=column_name_new))
             self.cursor.fetchall()
             self.conn.commit()
-            print("finished renaming column {} to {} in table {}".format(column_name_old,column_name_new,table))
+            print("finished renaming column {} to {} in table {} at time {}".format(column_name_old,column_name_new,table,time.asctime()))
         print("finished renaming all columns")
 
     def return_column(self,columns:List[str],table:str):
@@ -55,7 +55,7 @@ class utils(object):
         self.cursor.fetchall()
         self.delete_table([table])
         self.rename_table([[table+"_sorted",table]])
-        print("finished sorting table {}".format(table))
+        print("finished sorting table {} at time {}".format(table,time.asctime()))
 
     def sort_and_remove_duplicates(self,table_in,table_out,sortvar,idvar=None):
         query=self.query_bank["sort_and_remove_duplicates"]
@@ -67,25 +67,25 @@ class utils(object):
             self.rename_table([[table_in+"_sorted",table_in]])
         else:
             self.rename_table([[table_in+"_sorted",table_out]])
-        print("finished sorting and removing duplicates for table {}".format(table_in))
+        print("finished sorting and removing duplicates for table {} at time {}".format(table_in,time.asctime()))
 
     def duplicate_records(self,table_in,table_out,num):
         query=self.query_bank["duplicate_on"]
         self.cursor.execute(query.format(table_in=table_in,table_out=table_out,num=num))
         self.cursor.fetchall()
-        print("finishing duplicating records for table {} based on column {}".format(table_in,num))
+        print("finishing duplicating records for table {} based on column {} at time {}".format(table_in,num,time.asctime()))
 
     def create_index(self,table_name,index_name,column_name):
         query=self.query_bank["create_index"]
         self.cursor.execute(query.format(table_name=table_name,index_name=index_name,column_name=column_name))
         self.cursor.fetchall()
-        print("finishing creating index {} for table {} based on column {}".format(index_name,table_name,column_name))
+        print("finishing creating index {} for table {} based on column {} at time {}".format(index_name,table_name,column_name,time.asctime()))
 
     def drop_index(self,index_name):
         query=self.query_bank["drop_index"]
         self.cursor.execute(query.format(index_name=index_name))
         self.cursor.fetchall()
-        print("finishing dropping index {}".format(index_name))
+        print("finishing dropping index {} at time {}".format(index_name,time.asctime()))
 
     def change_column_type(self,table_name,column_name,column_type):
         query=self.query_bank["change_column_type"]
